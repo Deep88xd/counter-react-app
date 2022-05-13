3c02c8f7c711d3cd9d4a24273511fb6fad15f588
Deep88xd <98728227+Deep88xd@users.noreply.github.com>
2022-05-13 20:23:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import About from "./About";
-// import Contact from "./Contact";
-// import Navbar from "./Navbar";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a>Learn React</a>
-//       </header>
-//     </div>
-//   );
-// }
- 
-// export default App;
-
 import "./App.css";
 import Counter from "./Components/Counter";
 import "./Components/Counter.css";
